baa30ee49c77c51dadd4b97d0041aec3062e75f2
lokman-warrache <lokman.wrc@gmail.com>
2021-09-03 22:26:53 +0200

    Share code in github

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import data from './data';
+
+
+function App() {
+
+
+  return (
+   <div className="gird-content">
+  <header className="row">
+    <div>
+      <a className="brand" href="/">amazona</a>
+    </div>
+    <div>
+      <a href="/cart">Cart</a>
+      <a href="/signin">Sign In</a>
+    </div>
+  </header>
+  <main>
+    <div>
+    <div className="row center">
+
+    {data.products.map(product => (
+       <div key={product._id} className="card">
+       <a href={`/product/${product._id}`}>
+         <img className="medium" src={product.image} alt={product.name}/>
+       </a>
+       <div className="card-body">
+         <a href={`/product/${product._id}`}>
+           <h2>{product.name}</h2>
+         </a>
+         <div className="rating">
+           <span><i className="fa fa-star" /></span>
+           <span><i className="fa fa-star" /></span>
+           <span> <i className="fa fa-star" /></span>
+           <span><i className="fa fa-star" /></span>
+           <span><i className="fa fa-star" /></span>
+         </div>
+         <div className="price">
+           {product.price}$
+         </div>
+       </div>
+     </div>
+    ))
+    }
+      </div>
+    </div>
+  </main>
+  <footer className="row center">
+    All right reserved
+  </footer>
+</div>
+
+       
+  )
+}
+
+export default App;
