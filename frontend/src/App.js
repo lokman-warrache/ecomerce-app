0b5e7efae2ef8243b940e63fc11c32526f592510
lokman-warrache <lokman.wrc@gmail.com>
2021-09-08 13:21:56 +0200

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,118 @@
 import React from 'react';
-import data from './data';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
+import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import OrderScreen from './screens/OrderScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
 
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
 
   return (
+    <BrowserRouter>
    <div className="gird-content">
   <header className="row">
     <div>
-      <a className="brand" href="/">amazona</a>
+    <Link className="brand" to="/">
+              amazona
+     </Link>
     </div>
     <div>
-      <a href="/cart">Cart</a>
-      <a href="/signin">Sign In</a>
+    <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
     </div>
   </header>
   <main>
-    <div>
-    <div className="row center">
 
-    {data.products.map(product => (
-       <div key={product._id} className="card">
-       <a href={`/product/${product._id}`}>
-         <img className="medium" src={product.image} alt={product.name}/>
-       </a>
-       <div className="card-body">
-         <a href={`/product/${product._id}`}>
-           <h2>{product.name}</h2>
-         </a>
-         <div className="rating">
-           <span><i className="fa fa-star" /></span>
-           <span><i className="fa fa-star" /></span>
-           <span> <i className="fa fa-star" /></span>
-           <span><i className="fa fa-star" /></span>
-           <span><i className="fa fa-star" /></span>
-         </div>
-         <div className="price">
-           {product.price}$
-         </div>
-       </div>
-     </div>
-    ))
-    }
-      </div>
-    </div>
+    <Route path="/cart/:id?" component={CartScreen}></Route>
+    <Route path="/product/:id" component={ProductScreen} exact/>
+    <Route path="/" component={HomeScreen} exact/>
+    <Route path="/register" component={RegisterScreen}></Route>
+    <Route path="/shipping" component={ShippingAddressScreen}></Route>
+    <Route path="/payment" component={PaymentMethodScreen}></Route>
+    <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+    <Route path="/signin" component={SigninScreen}></Route>
+    <Route path="/order/:id" component={OrderScreen}></Route>
+    <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+    <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+    ></PrivateRoute>
+
   </main>
   <footer className="row center">
     All right reserved
   </footer>
 </div>
 
-       
+ </BrowserRouter>
   )
 }
 
---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,56 @@
+
+import Axios from 'axios';
+import {
+//   PRODUCT_CREATE_FAIL,
+//   PRODUCT_CREATE_REQUEST,
+//   PRODUCT_CREATE_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+//   PRODUCT_UPDATE_REQUEST,
+//   PRODUCT_UPDATE_SUCCESS,
+//   PRODUCT_UPDATE_FAIL,
+//   PRODUCT_DELETE_REQUEST,
+//   PRODUCT_DELETE_FAIL,
+//   PRODUCT_DELETE_SUCCESS,
+//   PRODUCT_CATEGORY_LIST_SUCCESS,
+//   PRODUCT_CATEGORY_LIST_REQUEST,
+//   PRODUCT_CATEGORY_LIST_FAIL,
+//   PRODUCT_REVIEW_CREATE_REQUEST,
+//   PRODUCT_REVIEW_CREATE_SUCCESS,
+//   PRODUCT_REVIEW_CREATE_FAIL,
+} from '../constants/productConstants';
+
+
+export const listProducts = () => async (dispatch) => {
+    
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+    });
+    try {
+      const { data } = await Axios.get(`/api/products`);
+      dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+    }
+  };
+
+
+  export const detailsProduct = (productId) => async (dispatch) => {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    try {
+      const { data } = await Axios.get(`/api/products/${productId}`);
+      dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
